8f4dc871911889da90e0f9fb33d9c82184fa97aa
1101-22 <alya.korobeynikova99@gmail.com>
2024-09-14 14:34:47 +0300

    Добавьте файлы проекта.

---
+++ 14.09/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Процедурно сгенерированный лабииринт");
+
+class Maze { 
+    public static Main(string[] args)
+    {
+        int width = 15;
+        int height = 15;
+
+        var maze = GenerateMaze;
+    }
+}
